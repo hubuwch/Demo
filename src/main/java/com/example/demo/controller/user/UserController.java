182316c9a559528dcbf4459ae9b4b1ecc29ff96c
weichao7573 <123456>
2017-12-18 17:33:56 +0800

    [A]新增消费者

--- src/main/java/com/example/demo/controller/user/UserController.java
+++ src/main/java/com/example/demo/controller/user/UserController.java
@@ -10,7 +10,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +22,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entry.User;
+import com.netflix.discovery.DiscoveryClient;
+
+
+
 
 /**
  * @title  UserController
@@ -30,10 +38,20 @@ public class UserController {
   
   static Map<Long, User> users = Collections.synchronizedMap(new HashMap<Long,User>());
   
+  private final Logger logger = Logger.getLogger(getClass());
+  
+ /* @Autowired
+  private DiscoveryClient client;*/
+  
+  @RequestMapping(value="/hello",method=RequestMethod.GET)
+  public String index(){
+    //ServiceInstance instance = client.getApplicationInfoManager();
+    return "hello";
+  }
   
   @RequestMapping(value="/",method=RequestMethod.GET)
   public List<User> getUserList(){
-    /*User user1 = new User();
+    User user1 = new User();
     user1.setId(1L);
     user1.setName("隗超");
     user1.setAge(12);
@@ -43,9 +61,9 @@ public class UserController {
     user2.setAge(25);
     List<User> users = new ArrayList<>();
     users.add(user1);
-    users.add(user2);*/
-    List<User> u = new ArrayList<>(users.values());
-    return u;
+    users.add(user2);
+    //List<User> u = new ArrayList<>(users.values());
+    return users;
   }
   
   @RequestMapping(value="/",method = RequestMethod.POST)
